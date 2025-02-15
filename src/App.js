61185c6497c6ccd98a4de5b9ab075343c78ec074
petriichuk <t.petriichuk@gmail.com>
2025-02-15 21:52:52 +0200

    added github actions

--- src/App.js
+++ src/App.js
@@ -23,3 +23,10 @@ function App() {
 }
 
 export default App;
+
+
+
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TJPHNHG2"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
